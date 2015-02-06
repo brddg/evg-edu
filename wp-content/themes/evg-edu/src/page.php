8a52eccb01754b85524c9dc1a2fc01d6794e77dc
John Pinkerton <jpinkerton88@gmail.com>
2015-02-06 20:51:39 +0000

    remove unnecessary markup from page template

--- wp-content/themes/evg-edu/src/page.php
+++ wp-content/themes/evg-edu/src/page.php
@@ -1,24 +1,13 @@
 <?php get_header(); ?>
 
-  <main role="main">
-    <!-- section -->
-    <section>
-
-      <h1><?php the_title(); ?></h1>
-
     <?php if (have_posts()): while (have_posts()) : the_post(); ?>
 
-      <!-- article -->
-      <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
         <?php the_content(); ?>
 
         <br class="clear">
 
         <?php edit_post_link(); ?>
-
-      </article>
-      <!-- /article -->
 
     <?php endwhile; ?>
 
@@ -34,8 +23,5 @@
 
     <?php endif; ?>
 
-    </section>
-    <!-- /section -->
-  </main>
 
 <?php get_footer(); ?>
