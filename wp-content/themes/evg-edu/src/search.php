7189a902aaa51f7b85de2cb4303e2e621322480f
John Pinkerton <jpinkerton88@gmail.com>
2015-02-07 21:42:17 +0000

    add search page. resolves #4

--- wp-content/themes/evg-edu/src/search.php
+++ wp-content/themes/evg-edu/src/search.php
@@ -1,19 +1,36 @@
 <?php get_header(); ?>
 
-	<main role="main">
-		<!-- section -->
-		<section>
+<div class="pages page-blog-list" id="page-blog-list">
+  <div class="container">
+    <!-- Header -->
+    <header>
+      <h4 class="line-divider"><?php echo sprintf( __( '%s Search Results for ', 'html5blank' ), $wp_query->found_posts ); echo get_search_query(); ?></h4>
+      <h1>Search</h1>
+      <div class="row">
+        <div class="span8 offset2">
+          <div class="input-append">
+            <form method="get" action="<?php echo home_url(); ?>" role="search">
+              <input name="s" class="span5" id="appendedInputButton" type="text" placeholder="Search Blog">
+              <button class="btn btn-primary sicon-search sicon-white" type="button"><i>Search</i></button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </header>
+    <!-- End Header -->
 
-			<h1><?php echo sprintf( __( '%s Search Results for ', 'html5blank' ), $wp_query->found_posts ); echo get_search_query(); ?></h1>
+    <div class="row-fluid">
+      <div class="span8 blog-details">
 
-			<?php get_template_part('loop'); ?>
+        <?php get_template_part('loop'); ?>
 
-			<?php get_template_part('pagination'); ?>
+        <?php get_template_part('pagination'); ?>
 
-		</section>
-		<!-- /section -->
-	</main>
-
-<?php get_sidebar(); ?>
+      </div>
+      
+      <?php get_sidebar(); ?>
+    </div>    
+  </div>
+</div>
 
 <?php get_footer(); ?>
